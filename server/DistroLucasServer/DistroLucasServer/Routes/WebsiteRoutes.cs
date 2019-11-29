55e53f6f269e27f5fc2f9ea6d094ebf3b73662cd
Lucas Assis <lucasassis@outlook.com>
2019-11-29 01:43:11 -0300

    Major updates - Paging system - CSS still needs work New routes for Custom page content

---
+++ server/DistroLucasServer/DistroLucasServer/Routes/WebsiteRoutes.cs
@@ -0,0 +1,62 @@
+﻿using Nucleus.Gaming;
+using Nucleus.Gaming.Web;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistroLucasServer {
+    [RouteManager("^/w")]
+    public class WebsiteRoutes {
+        private HttpResponse CustomPageRedirect(HttpRequest request, string pageName) {
+            Console.WriteLine($"{pageName} requested");
+
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            string host = request.Headers["Host"];
+
+            headers.Add("Connection", "keep-alive");
+            headers.Add("Date", DateTime.Now.ToString(CultureInfo.InvariantCulture));
+            headers.Add("Server", "distrolucas");
+            headers.Add("Location", $"http://{host}/?page={pageName}");
+
+            return new HttpResponse() {
+                ReasonPhrase = "OK",
+                StatusCode = "301",
+                ContentAsUTF8 = "",
+                Headers = headers
+            };
+        }
+
+        private HttpResponse CustomPage(HttpRequest request, string pageName) {
+            string language = "en";
+            string baseFolder = Path.Combine(AssemblyUtil.GetStartFolder(), "Resources", language);
+            string filePath = Path.Combine(baseFolder, pageName);
+
+            string txtPath = filePath + ".txt";
+            if (File.Exists(txtPath)) {
+                filePath = txtPath;
+            } else {
+                string htmlPath = filePath + ".html";
+                if (File.Exists(htmlPath)) {
+                    filePath = htmlPath;
+                }
+            }
+
+            FileInfo file = new FileInfo(filePath);
+            return new HttpResponse() {
+                ReasonPhrase = "",
+                StatusCode = "200",
+                ContentAsUTF8 = File.ReadAllText(file.FullName)
+            };
+        }
+
+        [Route("GET", "qs")]
+        public HttpResponse GetQuantumServant(HttpRequest request) {
+            return CustomPage(request, "qs");
+           
+        }
+    }
+}
--- www/a/i.js
+++ www/a/i.js
@@ -13,32 +13,53 @@ function parseLine(line) {
     return line;
 }
 
-nukeGet("/blog/bypage", (result) => {
-    var posts = result.split('<next>');
+var urlParams = new URLSearchParams(window.location.search);
+var page = urlParams.get("page");
+var requestPage;
+if (page == null || page == "blog") {
+    requestPage = "/blog/bypage";
+} else {
+    requestPage = `/w/${page}`;
+}
+
+nukeGet(requestPage, (result) => {
     var originalNode = document.getElementById("innerBlog");
     var parent = document.getElementById("blog");
-    for (var i = 0; i < posts.length; i++) {
-        bibCounter = 0;
 
-        var lines = posts[i].split('\n');
+    var pageIndex = result.indexOf('distroContent');
+    if (pageIndex >= 0 && pageIndex < 128) {
+        var pageContent = document.createElement("div");
+        pageContent.classList.add("innerBlog");
 
-        var newInner = originalNode.cloneNode(true);
-        newInner.id = "Post" + i;
-        newInner.classList.add("innerBlog");
-        var c = newInner.children[0];
-        c.children[1].innerHTML = lines[0];
-        c.children[2].innerHTML = lines[1];
-        c.children[4].innerHTML = lines[2];
+        var actualContent = document.createElement("div");
+        actualContent.classList.add("contentText");
+        actualContent.innerHTML = result;
+        pageContent.appendChild(actualContent);
 
-        var remaining = lines.length;
-        for (var j = 3; j < remaining; j++) {
-            var line = parseLine(lines[j]);
-            var span = document.createElement("p");
-            span.innerHTML = line;
-            span.className = "blogText";
-            c.children[3].appendChild(span);
+        parent.appendChild(pageContent);
+    } else {
+        var posts = result.split('<next>');
+        for (var i = 0; i < posts.length; i++) {
+            bibCounter = 0;
+            var lines = posts[i].split('\n');
+
+            var newInner = originalNode.cloneNode(true);
+            newInner.classList.add("innerBlog");
+            var c = newInner.children[0];
+            c.children[1].innerHTML = lines[0];
+            c.children[2].innerHTML = lines[1];
+            c.children[4].innerHTML = lines[2];
+
+            var remaining = lines.length;
+            for (var j = 3; j < remaining; j++) {
+                var line = parseLine(lines[j]);
+                var span = document.createElement("p");
+                span.innerHTML = line;
+                span.className = "blogText";
+                c.children[3].appendChild(span);
+            }
+            parent.appendChild(newInner);
         }
-        parent.appendChild(newInner);
     }
     originalNode.hidden = true;
 }, null);
